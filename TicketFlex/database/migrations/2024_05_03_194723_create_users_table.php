bd038b1e98dbbb64a7d660c9b6ebd6cadcc0d83a
ManuBln <manu320cr@gmail.com>
2024-05-05 18:14:47 +0200

    feat(TicketFlex):Tables users, compras, artistas y articulos created

---
+++ TicketFlex/database/migrations/2024_05_03_194723_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre')->require()->unique();
+            $table->string('correo')->unique()->require();
+            $table->string('password')->require();
+            $table->string('telefono');
+            $table->string('tipo_usuario');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
