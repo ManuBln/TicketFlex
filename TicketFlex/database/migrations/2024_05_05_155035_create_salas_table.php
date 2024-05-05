f38bf1a3a1df435be2e2affffa1bae4ae6565370
Pablo <pablomolinaesteban@gmail.com>
2024-05-05 18:11:05 +0200

    feat(TickeFlex):table salas created

---
+++ TicketFlex/database/migrations/2024_05_05_155035_create_salas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('salas', function (Blueprint $table) {
+            $table->id();
+            $table->string("nombre");
+            $table->integer("aforo");
+            $table->string("imagen");
+            $table->string("lugar");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salas');
+    }
+};
