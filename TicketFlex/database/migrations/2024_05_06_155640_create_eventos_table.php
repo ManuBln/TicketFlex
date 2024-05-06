fc824ba67c156f2bca2832b12076c7b8746889c8
pablo <pablomolinaesteban@gmail.com>
2024-05-06 18:25:14 +0200

    subida de tabla

---
+++ TicketFlex/database/migrations/2024_05_06_155640_create_eventos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eventos', function (Blueprint $table) {
+            $table->id();
+            $table->string("nombre_evento");
+            $table->decimal("precio", 8, 2);
+            $table->dateTime("fecha_hora");
+            $table->text("descripcion");
+            $table->integer("aforo");
+            $table->integer("aforo_disponible");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eventos');
+    }
+};
